1689b07b2bb126498daf968aa43c7b99b9ebe26f
10suda <82120104+10suda@users.noreply.github.com>
2021-08-07 13:53:31 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,37 +1,57 @@
 
-function createGrid(x = 256){
+function createGrid(x = gridSize*gridSize){
     const container = document.querySelector('.container');
+    const squares = document.querySelectorAll('.container div');
+    for (let i = 0; i < squares.length; i++)
+    {
+    container.removeChild(squares[i]);
+    }
     while(x!=0){
+        container.setAttribute("style", `grid-template-columns: repeat(${gridSize}, 1fr); grid-template-rows: repeat(${gridSize}, 1fr);`);
         container.appendChild(document.createElement('div'));
         x--;
     }
+    const newSquares = document.querySelectorAll('.container div');
+    newSquares.forEach((div) => {
+        div.addEventListener("mouseover", draw);
+    });
+    newSquares.forEach((div) => {
+        div.setAttribute("style", "background-color:white;")
+    });
 }
 let drawColor = "#000000";
 function draw() //modify the grid square attributes
 {
     this.setAttribute("style", `background-color:${drawColor};`)
 }
-const inputs = document.querySelector('input');
+
+let gridSize = 16;
+const inputs = document.querySelectorAll('input');
 function colorChange()
 {
-    drawColor = inputs.value;
+    drawColor = inputs[0].value;
+}
+
+function sizeChange()
+{
+    gridSize = inputs[1].value;
+    document.querySelector('label').innerHTML = `Grid Size = ${gridSize}x${gridSize}`;
+    createGrid(gridSize*gridSize);
 }
 
 createGrid();
-
-const squares = document.querySelectorAll('.container div');
-console.log(squares);
-squares.forEach((div) => {
-    div.addEventListener("mouseover", draw, false);
-});
-
 const buttons = document.querySelectorAll('button');
 buttons.forEach((button) => {
     button.addEventListener('click', () => {
       if (button.id == 'reset'){
+        const squares = document.querySelectorAll('.container div');
           squares.forEach((div) => {
               div.setAttribute("style", "background-color:white;")
           });
+          inputs[1].value = 16;
+          inputs[0].value = "#000000";
+          sizeChange();
+          colorChange();
       }
     });
   });
